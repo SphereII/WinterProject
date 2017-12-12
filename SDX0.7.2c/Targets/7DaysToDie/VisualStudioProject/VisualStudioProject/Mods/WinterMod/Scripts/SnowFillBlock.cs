e4a02ef9b4f1e8ef68b26d7aecea322b2fb0e86e
SphereII <support@7d2dlauncher.org>
2017-12-12 16:18:54 -0400

    updating repo

---
+++ SDX0.7.2c/Targets/7DaysToDie/VisualStudioProject/VisualStudioProject/Mods/WinterMod/Scripts/SnowFillBlock.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+
+// Custom SnowFill class that makes snow act like plants, for the purpose of snow to disappear in low-light situations.
+public class BlockSnowFill : BlockPlant
+{
+
+    // Stores when to do the next light check and what the current light level is
+    // Determines if they run or not.
+    private float nextCheck = 0;
+    byte lightLevel;
+
+    // Globa value for the light threshold in which zombies run in the dark
+    public static byte LightThreshold = 10;
+
+    // Frequency of check to determine the light level.
+    public static float CheckDelay = 1f;
+
+    public new int lightLevelStay = 10;
+
+    public int Counter = 0;
+    public override void Init()
+    {
+        base.Init();
+      //  this.IsRandomlyTick = false;
+        if (this.Properties.Values.ContainsKey("LightLevelStay"))
+        {
+            int.TryParse(this.Properties.Values["LightLevelStay"], out this.lightLevelStay);
+        }
+    }
+    //Vector3i blockPos = new Vector3i(num2, num, num3);
+
+    public override bool CanPlantStay(WorldBase _world, int _clrIdx, Vector3i _blockPos, BlockValue _blockValue)
+    {
+        return _world.GetBlockLightValue(_clrIdx, _blockPos) >= this.lightLevelStay; 
+    }
+
+    // Token: 0x06000044 RID: 68
+    public override bool CheckPlantAlive(WorldBase _world, int _clrIdx, Vector3i _blockPos, BlockValue _blockValue)
+    {
+        // Check, but only 1 out of 3 times.
+        if (!this.CanPlantStay(_world, _clrIdx, _blockPos, _blockValue)  )
+        {
+            _world.SetBlockRPC(_clrIdx, _blockPos, BlockValue.Air);
+           
+        }
+
+        return true;
+    }
+
+    /*
+     * 
+     * 				if (blockValue.type == 0 && num9 >= num10)
+				{
+					sbyte density2 = MarchingCubes.DensityTerrain;
+					_chunk.SetDensity(x, num10, z, density2);
+					long texture2 = this.GetTexture(i + num2, k, j + num6);
+					_chunk.SetTextureFull(x, num10, z, texture2);
+					_chunk.SetBlock(_world, x, num10, z, Block.GetBlockValue("snowFill"));
+				}
+                */
+
+
+
+}
---
+++ SDX0.7.2c/Targets/7DaysToDie/VisualStudioProject/VisualStudioProject/Mods/WinterMod/Scripts/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+// Custom SnowFill class that makes snow act like plants, for the purpose of snow to disappear in low-light situations.
+public class KittenSpawner : Block
+{
+
+   
+    public override void Init()
+    {
+        base.Init();
+ 
+    }
+
+
+    public override void OnBlockAdded(WorldBase world, Chunk _chunk, Vector3i _blockPos, BlockValue _blockValue)
+    {
+        int randomFromGroup =EntityGroups.GetRandomFromGroup("Nightmares");
+        Entity newEntity = EntityFactory.CreateEntity(randomFromGroup, new Vector3(0f, UnityEngine.Random.value * 360f, 0f)); 
+        _chunk.AddEntityToChunk(newEntity);
+            
+            
+    }
+
+
+    /*
+     * 
+     * 				if (blockValue.type == 0 && num9 >= num10)
+				{
+					sbyte density2 = MarchingCubes.DensityTerrain;
+					_chunk.SetDensity(x, num10, z, density2);
+					long texture2 = this.GetTexture(i + num2, k, j + num6);
+					_chunk.SetTextureFull(x, num10, z, texture2);
+					_chunk.SetBlock(_world, x, num10, z, Block.GetBlockValue("snowFill"));
+				}
+                */
+
+
+
+}
